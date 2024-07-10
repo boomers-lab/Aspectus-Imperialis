13d2c4e0f36348c160152469d652719065509b63
YasinAhmet <77017655+YasinAhmet@users.noreply.github.com>
2024-07-10 22:47:42 +0300

    Fireplace effect's source code

---
+++ Source/FireplaceAddOn/CompProperties_FireOverlaySouth.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+//namespace GW_Fireplace {﻿
+
+ public class Comp_FireOverlaySouth : CompFireOverlay
+    {
+		public override void PostDraw()
+		{
+			if ((refuelableComp == null || refuelableComp.HasFuel) && parent.Rotation == Rot4.South)
+			{
+				Vector3 drawPos = parent.DrawPos;
+				drawPos.y += 3f / 74f;
+				FireGraphic.Draw(drawPos, Rot4.North, parent);
+			}
+		}
+		public override void CompTick()
+		{
+			if (((refuelableComp == null || refuelableComp.HasFuel) && parent.Rotation == Rot4.South) && startedGrowingAtTick < 0)
+			{
+				startedGrowingAtTick = GenTicks.TicksAbs;
+			}
+		}
+	}
+
+    public class CompProperties_FireOverlaySouth : CompProperties_FireOverlay
+    {
+        public CompProperties_FireOverlaySouth()
+        {
+            compClass = typeof(Comp_FireOverlaySouth);
+        }
+
+    }
